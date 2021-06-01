2caa7f743fb6fa18f6203b4488fe8369506ba22a
Tijani Habeeb <79145763+habeeb10@users.noreply.github.com>
2021-06-01 17:54:17 +0100

    chore:refactor app screen

--- App.js
+++ App.js
@@ -1,8 +1,7 @@
 import "react-native-gesture-handler";
 import React from "react";
-import MainNavigator from "./Navigator/MainNavigation";
-import Welcome from "./screens/Welcome";
-import Navigator from "./Navigator/MainNavigation";
+
+import Navigator from "./src/Navigator/MainNavigation";
 
 export default function App() {
   return <Navigator />;
